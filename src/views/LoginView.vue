92604279b21a06904b8f25366b085b1e219bc0eb
Andressa Paranhos <andressaparanhos1@hotmail.com>
2023-01-09 19:11:15 -0300

    add: criação da página de login (ainda em andamento)

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,21 +1,59 @@
 <template>
   <div class="login-view">
-
+    <div class="box-login">
+      <img src="../assets/nextcode.png" alt="Nextcode" />
+      <NxTextbox label="E-mail" v-model="email" placeholder="Digite seu email"/>
+      <NxTextbox label="Senha" type="password" v-model="password" placeholder="Digite sua senha"/>
+      <NxButton @click="login">Login</NxButton>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 
 export default {
   components: {
+    NxTextbox: () => import('@/components/Textbox'),
+    NxButton: () => import('@/components/Button')
   },
   data () {
-    return {}
+    return {
+      email: '',
+      password: ''
+    }
   },
   methods: {
+    login () {
+      const dataForm = { email: this.email, password: this.password }
+      console.log(dataForm)
+      const response = axios.post('http://localhost:8080/login', {
+        email: dataForm.email,
+        password: dataForm.password
+      })
+      if (response.status === 200) {
+        localStorage.setItem('user', response.data.user)
+      }
+      console.log(response)
+    }
   }
 }
 </script>
+<style lang="scss" scoped>
+.login-view {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
 
-<style scoped>
+.box-login {
+  width: 400px;
+  height: 400px;
+  padding: 1.8rem;
+  margin: 0 auto;
+  background-color: themeColor(mainBg);
+  color: white;
+}
+
 </style>
